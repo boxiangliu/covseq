a5433d38ecd72212018c18b8f42e53cad8d2ca14
Boxiang Liu <jollier.liu@gmail.com>
2020-03-09 11:39:48 -0700

    changed coviz.py"

--- coviz.py
+++ coviz.py
@@ -2,7 +2,7 @@ import click
 from datetime import date
 import time
 from annotate import annotate
-
+from orf_prediction import predict
 @click.group()
 def coviz():
 	pass
@@ -17,9 +17,16 @@ def annotate_interface(fasta, out_dir):
 	click.echo(f"Output directory: {out_dir}")
 	annotate(fasta, out_dir)
 
-# @coviz.command()
-# def predict():
-# 	pass
+@coviz.command()
+@click.option("-f", "--fasta", type=str, required=True, \
+	help="Fasta file containing one or more virus strains.")
+@click.option("-o", "--out_dir", type=str, required=False, \
+	help="Output directory", default="results", show_default=True)
+def predict_inferface():
+	click.echo(f"FASTA file: {fasta}")
+	click.echo(f"Output directory: {out_dir}")
+	predict(fasta, out_dir)
+
 
 # @coviz.command()
 # def phylogeny():
---
+++ orf_prediction.py
@@ -0,0 +1,9 @@
+def predict(fasta_fn, out_dir):
+	run_glimmer3(fasta_fn, out_dir)
+	clean(in_dir, out_dir)
+
+def run_glimmer3(fasta_fn, out_dir):
+	cmd = 
+
+def clean(in_dir, out_dir):
+	pass
