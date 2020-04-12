5139ad469e7a855f28ea135b4b11e03514c1a1a5
Boxiang Liu <jollier.liu@gmail.com>
2020-04-11 17:50:59 -0700

    updated preprocess/ and website/

--- preprocess/concatenate_fasta.py
+++ preprocess/concatenate_fasta.py
@@ -1,23 +1,58 @@
 import glob
+import os
 import gzip
 from Bio import SeqIO
 in_dir = "../data/"
-sub_dirs = ["cngb", "embl", "gisaid", "ncbi"]
+sub_dirs = ["gisaid", "ncbi", "cngb", "embl"]
 out_fn = "../data/aggregated/fasta/raw.fasta"
+cgnb_metadata = "../data/cngb/metadata/CNGBdb_VirusDIP_excel20200411_all(24)_57b4ce53c4d6c49c978596677a112211.csv"
 
 print("#########################")
 print("# Concatenate all FASTA #")
 print("#########################")
 print(f"Input: {in_dir}")
 print(f"Output: {out_fn}")
-counter = 0
-with open(out_fn, "w") as fout:
-	for sd in sub_dirs:
-		wd = f"{in_dir}/{sd}/fasta/"
-		print(f"Working on directory {wd}")
-		for fn in glob.glob(f"{wd}/*.fasta"):
-			for record in SeqIO.parse(fn, "fasta"):
-				SeqIO.write(record, fout, "fasta")
-				counter += 1
-print(f"Combined {counter} sequences!")
 
+
+def get_cngb_id_map(in_fn):
+	id_map = {}
+	with open(in_fn) as f:
+		for line in f:
+			if "Sequence ID" in line:
+				continue
+			split_line = line.split(",")
+			fasta_fn = os.path.basename(split_line[-2]).replace(".gz", "")
+			ID = split_line[0]
+			id_map[fasta_fn] = ID
+	return id_map
+
+
+def concatenate_fasta(in_dir, sub_dirs, out_fn, cngb_id_map):
+	counter = 0
+	with open(out_fn, "w") as fout:
+		for sd in sub_dirs:
+			wd = f"{in_dir}/{sd}/fasta/"
+			print(f"Working on directory {wd}")
+			for fn in glob.glob(f"{wd}/*.fasta"):
+				for record in SeqIO.parse(fn, "fasta"):
+					if sd == "gisaid":
+						seq_header = record.description.split("|")[1].strip()
+					elif sd == "ncbi":
+						seq_header = record.description.split("|")[0].strip()
+					elif sd == "embl":
+						seq_header = record.description.split("|")[1].strip()
+					elif sd == "cngb":
+						bn = os.path.basename(fn)
+						seq_header = cngb_id_map[bn]
+					record.id = record.name = record.description = seq_header
+					SeqIO.write(record, fout, "fasta")
+					counter += 1
+	print(f"Combined {counter} sequences!")
+
+
+def main():
+	cngb_id_map = get_cngb_id_map(cgnb_metadata)
+	concatenate_fasta(in_dir, sub_dirs, out_fn, cngb_id_map)
+
+if __name__ == "__main__":
+	main()
--- preprocess/filter_fasta.py
+++ preprocess/filter_fasta.py
@@ -1,4 +1,5 @@
 import os
+import hashlib
 from Bio import SeqIO
 
 in_fn = "../data/aggregated/fasta/raw.fasta"
@@ -12,17 +13,60 @@ print("#############################")
 print(f"Input: {in_fn}")
 print(f"Output: {out_fn}")
 
-print("Getting sequence lengths.")
-with open(f"{out_dir}/genome_lengths.tsv", "w") as fout:
-	for record in SeqIO.parse(in_fn, "fasta"):
-		length = len(record.seq)
-		fout.write(f"{record.description}\t{length}\n")
 
-print("Filtering for complete genomes.")
-cutoff = 25000
-with open(f"{out_dir}/complete_genomess.fasta", "w") as fout:
-	for record in SeqIO.parse(in_fn, "fasta"):
-		length = len(record.seq)
-		if length >= cutoff:
-			SeqIO.write(record, fout, "fasta")
+def remove_duplicates(in_fn, out_dir):
+	print("Removing duplicates.")
+	hash_set = set()
+	with open(f"{out_dir}/duplicated.tsv", "w") as out_table, \
+		open(f"{out_dir}/non_redundant.fasta", "w") as out_fasta:
+		out_table.write("ID\tduplicated\n")
 
+		for record in SeqIO.parse(in_fn, "fasta"):
+			sha256 = hashlib.sha256(str(record.seq).encode("utf-8")).hexdigest()
+			if sha256 not in hash_set:
+				hash_set.add(sha256)
+				out_table.write(f"{record.description}\t0\n")
+				SeqIO.write(record, out_fasta, "fasta")
+			else:
+				out_table.write(f"{record.description}\t1\n")
+
+
+def get_genome_length(in_fn, out_dir):
+	print("Getting sequence lengths.")
+	with open(f"{out_dir}/genome_lengths.tsv", "w") as fout:
+		for record in SeqIO.parse(in_fn, "fasta"):
+			length = len(record.seq)
+			fout.write(f"{record.description}\t{length}\n")
+
+
+def filter_complete_genome(in_fn, out_dir):
+	print("Filtering for complete genomes.")
+	cutoff = 25000
+	with open(f"{out_dir}/complete_genome.fasta", "w") as fout:
+		for record in SeqIO.parse(in_fn, "fasta"):
+			length = len(record.seq)
+			if length >= cutoff:
+				SeqIO.write(record, fout, "fasta")
+
+
+def count_ambiguous_base(in_fn, out_dir):
+	print("Count ambiguous bases.")
+	with open(f"{out_dir}/ambiguous_bases.tsv", "w") as fout:
+		for record in SeqIO.parse(in_fn, "fasta"):
+			ambi_base = 0
+			length = len(record.seq)
+			for b in str(record.seq):
+				if b.upper() not in "ATCGU":
+					ambi_base += 1
+			proportion = ambi_base / length
+			fout.write(f"{record.description}\t{ambi_base}\t{length}\t{proportion}\n")
+
+
+def main():
+	remove_duplicates(in_fn, out_dir)
+	# get_genome_length(in_fn, out_dir)
+	# filter_complete_genome(in_fn, out_dir)
+	# count_ambiguous_base(f"{out_dir}/complete_genome.fasta", out_dir)
+
+if __name__ == "__main__":
+	main()
