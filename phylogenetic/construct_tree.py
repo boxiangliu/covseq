ba916b6bb7d124d45fd1b05f8557826da3d8bfff
Boxiang Liu <jollier.liu@gmail.com>
2020-06-29 15:09:19 -0700

    small fix

--- phylogenetic/construct_tree.py
+++ phylogenetic/construct_tree.py
@@ -161,7 +161,7 @@ def main(in_fn, out_prefix, make_tree, mafft_mode, ref_fasta, tree_threads):
 	print(f"Output prefix: {out_prefix}")
 
 	out_dir = os.path.dirname(out_prefix)
-	os.makedir(out_dir, exist_ok=True)
+	os.makedirs(out_dir, exist_ok=True)
 
 	msa(mode=mafft_mode, \
 		fasta_fn=in_fn, \
