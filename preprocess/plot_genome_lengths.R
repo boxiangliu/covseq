40670e205332a0df095a4a6f7926de0ff3d493c6
Boxiang Liu <jollier.liu@gmail.com>
2020-09-13 18:01:10 -0700

    modified:   preprocess/plot_genome_lengths.R

--- preprocess/plot_genome_lengths.R
+++ preprocess/plot_genome_lengths.R
@@ -8,11 +8,12 @@ if (!dir.exists(out_dir)) {dir.create(out_dir)}
 genome_lengths = fread(in_fn, sep="\t", col.names = c("description", "length"))
 p = ggplot(genome_lengths, aes(x=length)) + 
 	geom_histogram(bins=100) + 
-	scale_y_sqrt(breaks=c(0,100,1000,2500,5000,7500,10000,12500)) + 
+	scale_y_sqrt() + 
 	geom_vline(xintercept = 25000, color="red", linetype="dashed") + 
 	scale_x_continuous(breaks=c(0,5000,10000,15000,20000,25000,30000)) +
 	xlab("SARS-CoV-2 Sequence Lengths") + 
-	ylab("Frequency")
+	ylab("Frequency") + 
+    theme_classic()
 print(p)
 
 save_plot(sprintf("%s/genome_lengths.pdf", out_dir), p, base_width=6, base_height=3)
